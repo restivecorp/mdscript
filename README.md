6e3d053ed7f9b16a703e046acba830c3d5aa6467
RestiveCorp <restive.corp@gmail.com>
2020-05-16 19:28:34 +0200

    Rename file options #1

--- README.md
+++ README.md
@@ -20,3 +20,18 @@ Metadata Script
     $ python3 mds.py --ver
     $ python3 mds.py --ver --verbose
 ```
+
+### Rename
+```
+    # Rename files .jpg start at 0001
+    $ python3 mds.py --rename /potos/album/ 
+
+    # Rename files .avi start at 0001
+    $ python3 mds.py --rename /potos/album/ --ext avi
+
+    # Rename files .avi start with prefix v0001
+    $ python3 mds.py --rename /potos/album/ --ext avi --pfx v
+
+    # Rename files .avi start with prefix in counter v0005
+    $ python3 mds.py --rename /potos/album/ --ext avi --pfx v --idx 5
+```
--- mds.py
+++ mds.py
@@ -1,29 +1,86 @@
 import sys
 import os
+import glob
 import argparse
 
 # Application version
 version = "1.0-alpha"
 verbose = False
+extension = "jpg"
+start = 0
+
+def rename(path, ext, idx, pfx):
+    os.chdir(path)
+
+    if ext is None:
+        ext = extension
+    else:
+        ext = ext.replace("*","")
+        ext = ext.replace(".","")
+
+    if idx is None:
+        idx = start
+    else:
+        try:
+            idx = int(idx)
+        except:
+            print("idx must be a numbrer")
+            sys.exit()
+
+    if pfx is None:
+        pfx = ""
+
+    if verbose:
+        print("  Path: {}".format(os.getcwd()))
+        print("  Extension: {}".format(ext))
+        print("  Start: {}".format(idx))
+        print("  Prefix: {}".format(pfx))
+        print("  Recursive: {}".format(r))
+    
+    if idx == 0:
+        count = 1
+    else:
+        count = idx
+    
+    total_renamed = 0
+
+    for filename in glob.glob("*." + ext):
+        renamed = pfx + str(count).zfill(4) + "." + ext
+        os.rename(filename, renamed)
+        count += 1
+        total_renamed += 1        
+
+    print("Renamed {} files!".format(total_renamed))
+    
+
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser("Help from the script to establish metadata.")
+    parser = argparse.ArgumentParser("Help from the script to establish metadata.")
 
-	parser.add_argument("--ver", help="Show the version of the program", action="store_true")
-	parser.add_argument("--verbose", help="Shows detailed information of the processes", action="store_true")
+    parser.add_argument("--version", help="Show the version of the script", action="store_true")
+    parser.add_argument("--verbose", help="Shows detailed information of the processes", action="store_true")
+    parser.add_argument("--rename", help="Rename PATH files. Default: 0001.jpg", metavar="PATH")
+    parser.add_argument("--ext", help="Extension of the files to be processed. Default: jpg", metavar="EXTENSION")
+    parser.add_argument("--idx", help="Start number to rename. Default: 0001", metavar="START")
+    parser.add_argument("--pfx", help="Prefix to rename. Default: none", metavar="PREFIX")
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
     # verbose
-	if args.verbose:
-		verbose = True
-		if verbose:
-			print("Verbose mode is on.")
-	
+    if args.verbose:
+        verbose = True
+        if verbose:
+            print("Verbose mode is on.")
+
     # ver
-	if args.ver:
-		if verbose:
-			print("The script version is: {}".format(version))
-		else:
-			print(version)
-		sys.exit()
+    if args.version:
+        if verbose:
+            print("The script version is: {}".format(version))
+        else:
+            print("Version: ", version)
+        sys.exit()
+
+    # rename
+    if args.rename:
+        rename(args.rename, args.ext, args.idx, args.pfx)
+        sys.exit()
